84d6712b4d3ba33681d6a9b52290b65f1d01d22e
Herbert Nicholas <125560895+henite2005@users.noreply.github.com>
2023-09-27 15:10:41 +0300

    SQL_SCRIPT_TO_FIND_PORT_NUMBER_OF_THE_DATABASE.sql

---
+++ SQL_SCRIPT_TO_FIND_PORT_NUMBER_OF_THE_DATABASE.sql
@@ -0,0 +1,14 @@
+---SQL SCRIPT TO FIND PORT NUMBER OF THE DATABASE---
+
+SET NOCOUNT ON
+DECLARE @port varchar(30), @key varchar(150)
+IF charindex('\',@@servername,0) <>0
+BEGINSET @key = 'SOFTWARE\MICROSOFT\Microsoft SQL Server\'
++@@servicename+'\MSSQLServer\Supersocketnetlib\TCP'
+END
+ELSE
+BEGINSET @key = 'SOFTWARE\MICROSOFT\MSSQLServer\MSSQLServer \Supersocketnetlib\TCP'
+ENDEXEC master..xp_regread @rootkey='HKEY_LOCAL_MACHINE',
+@key=@key,@value_name='Tcpport',@value=@port OUTPUT
+
+SELECT 'SQL Server Name: '+@@servername + ' Port # '+convert(varchar(20),@port)
